5561976ff6ab56685f1fc43b908768d6c8c7b384
CongHienn <doanconghienmta2016@gmail.com>
2022-10-08 08:57:20 +0700

    Day code

--- js/main.js
+++ js/main.js
@@ -6,4 +6,9 @@ console.log("Tien luong nhan vien =" + tienLuong);
 //Bai 2: Tính trung bình
 var num1=5, num2=5,num3=4,num4=5,num5=4;
 var tinhTrungBinh=(num1+num2+num3+num4+num5)/5;
-console.log("Trung binh 5 so= " + tinhTrungBinh);git
+console.log("Trung binh 5 so= " + tinhTrungBinh);
+
+//Bài 3: Quy đổi tiền
+var soTienUSD= 50;
+var soTienQuyDoi=soTienUSD*23500;
+console.log("So tien quy doi = " + soTienQuyDoi);
